f6571888e1ec6bcf56e0a9d16381b83f949b31b6
Ozsie <oscar@djupfeldt.se>
2018-04-19 21:29:12 +0200

    Started work on tests

--- src/test/kotlin/com/github/ozsie/webfluxtest/WebfluxTestApplicationTests.kt
+++ src/test/kotlin/com/github/ozsie/webfluxtest/WebfluxTestApplicationTests.kt
@@ -1,16 +1,68 @@
 package com.github.ozsie.webfluxtest
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import com.github.ozsie.webfluxtest.model.Item
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.server.RouterFunction
 
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest
 class WebfluxTestApplicationTests {
 
-	@Test
-	fun contextLoads() {
+	private lateinit var client: WebTestClient
+
+	private var expectedList: MutableList<Item> = mutableListOf()
+
+	@Autowired
+	private lateinit var repository: ItemRepository
+
+	@Autowired
+	private lateinit var routes: RouterFunction<*>
+
+	@BeforeEach
+	fun before() {
+        init(repository)
+		client = WebTestClient
+				.bindToRouterFunction(routes)
+				.configureClient()
+				.baseUrl("/api")
+				.build()
+        repository.findAll().collectList().block() ?: mutableListOf()
 	}
+
+	@Test
+	fun testGetItems() {
+        expectedList = repository.findAll().collectList().block() ?: mutableListOf()
+		client
+                .get()
+				.uri("/items")
+				.exchange()
+				.expectStatus()
+				.isOk
+				.expectBodyList(Item::class.java)
+                .isEqualTo<WebTestClient.ListBodySpec<Item>>(expectedList)
+	}
+
+    @Test
+    fun testGetItem() {
+        expectedList = repository.findAll().collectList().block() ?: mutableListOf()
+        val item = expectedList[0]
+        val x = client
+                .get()
+                .uri("/item/${item.id}")
+                .exchange()
+                .expectStatus()
+                .isOk
+                .expectBody(Item::class.java)
+                .returnResult()
+
+        Assertions.assertEquals(item, x.responseBody)
+    }
 
 }
